9421189a6da49e3bc67f8b103e8ce43f32438c02
LinuxDinoGitHub <95269147+LinuxDinoGitHub@users.noreply.github.com>
2024-07-23 13:32:15 +0800

    Added jumping

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-let header = document.querySelector("h1");
-header.addEventListener("click",()=>{alert(header.innerHTML)})
+const dino = document.getElementById("dino");
+
+const dinoSprite = ["./Assests/dino1.png","./Assests/dino2.png"]
+setInterval(() => {
+    dino.setAttribute("src",dinoSprite[(dinoSprite.indexOf(dino.getAttribute("src"))+1)%2])
+}, 100);
+function jump(){
+    dino.classList.add("jump");
+    setTimeout(() => {
+        dino.classList.remove("jump");
+    }, 500);
+}
+
+document.addEventListener("keydown", (e) => {jump()});

3a1c7f0a666d082e2caabd4f797bc932cc4fd2eb
LinuxDinoGitHub <95269147+LinuxDinoGitHub@users.noreply.github.com>
2024-07-25 23:19:31 +0800

    added collision logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
 
-const dinoSprite = ["./Assests/dino1.png","./Assests/dino2.png"]
+const dinoSprite = ["./Assests/dino1.png","./Assests/dino2.png"];
 setInterval(() => {
     dino.setAttribute("src",dinoSprite[(dinoSprite.indexOf(dino.getAttribute("src"))+1)%2])
 }, 100);
@@ -12,5 +13,14 @@ function jump(){
         }, 700);
     }
 }
+
+let isAlive = setInterval(()=>{
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusSide = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+    let cactusTop = parseInt(window.getComputedStyle(cactus).getPropertyValue("top"));
+    if(cactusSide < 20 && cactusSide > 0 && dinoTop >= cactusTop){
+        console.log("hit")
+    }
+},20)
 
 document.addEventListener("keydown", (e) => {jump()});
